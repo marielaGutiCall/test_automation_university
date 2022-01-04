0d9e62e981c9b6cf5c8da81662e38b956a92331f
marielaGutiCall <mariela.guticall@gmail.com>
2022-01-04 17:02:06 -0400

    Unit -2: Web Drivers, Finding Web Elements

--- Set_Up/src/test/java/base/BaseTests.java
+++ Set_Up/src/test/java/base/BaseTests.java
@@ -1,8 +1,12 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class BaseTests {
 
@@ -21,8 +25,30 @@ public class BaseTests {
         //_driver.manage().window().fullscreen();
 
         // 3. Specific width and height (Show Crome iPhoneX emulator)
-        Dimension dimension = new Dimension(375, 812);
-        _driver.manage().window().setSize(dimension);
+        //Dimension dimension = new Dimension(375, 812);
+        //_driver.manage().window().setSize(dimension);
+
+
+
+        // Finding Web Elements, multiples Elements with findElements use data type List
+        List<WebElement> inputLinks = _driver.findElements(By.tagName("a"));
+        System.out.println(inputLinks.size());
+
+        // Finding Web Element, one Element with findElement
+        //WebElement inputLink = _driver.findElement(By.linkText("Inputs"));
+        //inputLink.click();
+
+        /*Optional Independent Exercise*/
+        WebElement scLink = _driver.findElement(By.linkText("Shifting Content"));
+        scLink.click();
+        List<WebElement> fmeLink = _driver.findElements(By.tagName("a"));
+        System.out.println(fmeLink.get(1).getText());
+        fmeLink.get(1).click();
+        List<WebElement> linksMenu = _driver.findElements(By.tagName("li"));
+        System.out.println(linksMenu.size());
+        for(WebElement itemMenu: linksMenu){
+            System.out.println("Item " + itemMenu.getText());
+        }
 
         _driver.quit();
 
